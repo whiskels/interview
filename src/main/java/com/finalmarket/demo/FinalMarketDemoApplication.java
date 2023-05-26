d10d5462088feca018ca87ca0fc44f41a72a0408
Maksim Kuzmin <kuzmingeo@gmail.com>
2023-05-26 13:50:36 +0300

    Initial commit

---
+++ src/main/java/com/finalmarket/demo/FinalMarketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.finalmarket.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalMarketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalMarketDemoApplication.class, args);
+	}
+
+}
